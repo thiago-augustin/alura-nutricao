588b59ae87794fdee1cee0fc0bd66cc15d0d1604
thiago-augustin <75146536+thiago-augustin@users.noreply.github.com>
2022-05-10 11:48:05 -0300

    Trabalhando com DOM e validações

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,33 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+if (peso <= 0 || peso >= 1000){
+    console.log("Peso inválido")
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválida";
+}
+
+if (altura <= 0 || altura >= 4.00){
+    console.log("Altura inválida")
+    alturaEhValido = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if (pesoEhValido && alturaEhValido){
+    var imc = peso/(altura*altura);
+    tdImc.textContent = imc;
+}
+
